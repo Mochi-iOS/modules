27f4b11dc59c245d54b4f9859233aef771a76d6d
Mochi-iOS <mochi.ios@proton.me>
2024-07-06 13:23:47 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,10 +3,10 @@ import { RepoMetadata } from "@mochiapp/js";
 export * from './src/turkish123'
 
 let metadata: RepoMetadata = {
-    name: "Babyyoda's Repo",
-    author: "babyyoda777",
+    name: "Some Repo",
+    author: "someone",
   //  icon: "https://raw.githubusercontent.com/Babyyoda777/babyyoda777.github.io/77c528f4fb69cbddf155121cc5b7932abf7c23f4/img/me.png",
-    description: "Babyyoda's Repo"
+    description: "someone Repo"
 }
 
 export default metadata;
